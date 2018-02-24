6282d9acfdd6b8b8c61905c20b9f1d686ea7f1a5
UsmanHaider15 <uhaider.bese15seecs@seecs.edu.pk>
2018-02-25 02:57:16 +0500

    Table Row Layout added in Items Details Activity

--- app/src/main/java/com/kindred/kindred/ItemsDetailsActitvity.java
+++ app/src/main/java/com/kindred/kindred/ItemsDetailsActitvity.java
@@ -1,9 +1,14 @@
 package com.kindred.kindred;
 
+import android.content.Context;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +40,9 @@ public class ItemsDetailsActitvity extends AppCompatActivity {
     String Price;
     String Note;
 
+    LinearLayout container;
+
+
 
     //Firebase Variables
     private DatabaseReference mItemsDatabase;
@@ -51,15 +59,15 @@ public class ItemsDetailsActitvity extends AppCompatActivity {
 
         final String post_id = getIntent().getStringExtra("post_id");
 
-        mItemsName = (TextView) findViewById(R.id.itemDetails_itemNames_textView);
-        mItemsQuantity = (TextView) findViewById(R.id.itemDetails_itemsQuantity_textView);
-        mItemsPrice = (TextView) findViewById(R.id.itemDetails_itemsPrice_textView);
-        mItemsNote = (TextView) findViewById(R.id.itemDetails_itemsNote_textView);
-
         mDropOffLocation = (TextView) findViewById(R.id.ItemsDetails_DropOffLocation_textView);
         mPurchasingLocation = (TextView) findViewById(R.id.itemsDetails_PurchasingLocation_textView);
         mDeliverUntil = (TextView) findViewById(R.id.ItemsDetails_DeliverUntil_textView);
         mServiceCharges = (TextView) findViewById(R.id.ItemsDetails_ServiceCharges_textView);
+
+        container = (LinearLayout) findViewById(R.id.Items_details_table_rows_container);
+        final ViewGroup finalContainer = container;
+
+
 
 
         final HashMap<String, String> itemMap = new HashMap<String, String>();
@@ -80,15 +88,34 @@ public class ItemsDetailsActitvity extends AppCompatActivity {
                 Iterable<DataSnapshot> itemsSnapshot = dataSnapshot.child("items").getChildren();
                 for(DataSnapshot item: itemsSnapshot)
                 {
+
+                    LayoutInflater layoutInflater =
+                            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    final View addView = layoutInflater.inflate(R.layout.items_detail_table_row, null);
+
+                    TextView Item_Name_TextView1 = (TextView) addView.findViewById(R.id.items_details_itemName) ;
+                    TextView Item_Quantity_TextView1 = (TextView) addView.findViewById(R.id.items_details_itemQuantity) ;
+                    TextView Item_Price_TextView = (TextView) addView.findViewById(R.id.Items_details_itemPrice) ;
+                    TextView Item_Note_TextView = (TextView) addView.findViewById(R.id.items_details_itemNote) ;
+                    
                     item_name[0] = (String)item.child("Item-Name").getValue();
-                    mItemsName.append(item_name[0] +"\n");
+                    Item_Name_TextView1.setText(item_name[0]);
+//                    mItemsName.append(item_name[0] +"\n");
                     item_quantity[0] = (String)item.child("Item-Quantity").getValue();
-                    mItemsQuantity.append(item_quantity[0] +"\n");
+                    Item_Quantity_TextView1.setText(item_quantity[0]);
+//                    mItemsQuantity.append(item_quantity[0] +"\n");
                     item_price[0] = (String)item.child("Item-Price").getValue();
-                    mItemsPrice.append(item_price[0] +"\n");
+                    Item_Price_TextView.setText(item_price[0]);
+//                    mItemsPrice.append(item_price[0] +"\n");
                     item_note[0] = (String)item.child("Item-Note").getValue();
-                    mItemsNote.append(item_note[0] +"\n");
+                    Item_Note_TextView.setText(item_note[0]);
+//                    mItemsNote.append(item_note[0] +"\n");
+
+                    finalContainer.addView(addView);
+
+
                 }
+
             }
 
             @Override
