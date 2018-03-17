3ec9e4eac4a37f81af769265938db0cd917dd003
UsmanHaider15 <45hl945hl9>
2018-03-17 15:45:47 +0500

    Continue with Facebook feature added

--- app/src/main/java/com/kindred/kindred/RegistrerActivity.java
+++ app/src/main/java/com/kindred/kindred/RegistrerActivity.java
@@ -1,5 +1,6 @@
 package com.kindred.kindred;
 
+import android.accounts.Account;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Handler;
@@ -9,16 +10,25 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
@@ -48,6 +58,11 @@ public class RegistrerActivity extends AppCompatActivity {
     private ConstraintLayout mConstraintLayout;
     private ProgressBar mProgressBar;
     private Handler mHandler = new Handler();
+
+    //Facebook login variables
+    private CallbackManager mCallbackManager;
+    private static final String TAG = "FACELOG";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,6 +112,32 @@ public class RegistrerActivity extends AppCompatActivity {
                 register_user(display_name, email, password);
             }
         });
+
+        //Facebook login
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.button_facebook_login);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+
     }
 
     private void register_user(final String display_name, String email, String password) {
@@ -163,4 +204,57 @@ public class RegistrerActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null)
+        {
+            updateUI(currentUser);
+        }
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        Toast.makeText(RegistrerActivity.this,"you are logged in", Toast.LENGTH_LONG).show();
+        Intent accountIntent = new Intent(RegistrerActivity.this, SetAvatarActivity.class);
+        startActivity(accountIntent);
+        finish();
+    }
+
+
+    //Facebook login methods
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(RegistrerActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
